18b2c1f7d52f8b70c037979c64ca400fcebd8d3f
shreyu003 <actoscriptdeveloper12@gmail.com>
2024-09-24 19:52:58 +0530

    select ticket page done by shreyu

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,21 @@ $(".head").click(function () {
     $(this).parent().find(".arrow").toggleClass("arrow-animate");
     $(this).parent().find(".highlightcontent").slideToggle(280);
 });
+
+
+
+// share button for product page
+
+function shareContent() {
+    if (navigator.share) {
+        navigator.share({
+            title: 'Share the link with friends and family members.',
+            text: 'This is an awesome webpage, you should visit it.',
+            url: window.location.href
+        })
+        .then(() => console.log('Successfully shared'))
+        .catch((error) => console.error('Error sharing', error));
+    } else {
+        alert('Web Share API is not supported in your browser.');
+    }
+}
